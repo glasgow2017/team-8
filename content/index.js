829a437fb3f6403e58039dce9d8e289818236c3e
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-04 19:11:27 +0000

    Replace "start reading" button by link and add details to todo

--- content/index.js
+++ content/index.js
@@ -1,9 +1,9 @@
 /**
  * @todo fix not working when focus in text field
- * @todo fix some elements having higher tabbing priority than button
+ * @todo fix some elements having higher tabbing priority than button (e.g. GitHub)
  */
 $('body').ready( function () {
-  $('body').prepend('<div id="screen-reader-summary"><button id="gsw17-8-sr-read">Start reading</button></div>');
+  $('body').prepend('<div id="screen-reader-summary"><a href="#scb-end" id="gsw17-8-sr-read">Read page from the start</a></div><div id="scb-end"></div>');
   
   $('#screen-reader-summary').focus();
 
