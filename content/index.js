9cbc0e1982971ef6d71dc32e1ad5cf80486c6112
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-04 19:08:00 +0000

    Add hotkey to screen reader banner button

--- content/index.js
+++ content/index.js
@@ -1,6 +1,13 @@
-
+/**
+ * @todo fix not working when focus in text field
+ * @todo fix some elements having higher tabbing priority than button
+ */
 $('body').ready( function () {
-  $('body').prepend('<p id="screen-reader-summary">This is placeholder screen-reader text which will be used to display the summary of the page.</p>');
+  $('body').prepend('<div id="screen-reader-summary"><button id="gsw17-8-sr-read">Start reading</button></div>');
   
   $('#screen-reader-summary').focus();
+
+  $(document).bind('keyup', 'ctrl+y', function () {
+    $('#gsw17-8-sr-read').focus();
+  });
 });
