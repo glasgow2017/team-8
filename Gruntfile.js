6cdb6407126d715f70dc5a631ea1bd63d22ace1c
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-04 22:45:39 +0000

    Add Grunt script to automatically mifiny JavaScript files into one file

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {
+          'content/script.min.js': [
+            'dev-js/add_top_banner.js',
+            'dev-js/extract_important_sentence.js',
+            'dev-js/index.js'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify']);
+};
