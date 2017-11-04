570870be9d4164f6a3866dd9f7849a574fc6565a
Seamus Salt <seamussalt@gmail.com>
2017-11-04 20:27:04 +0000

    Slight changes

--- content/index.js
+++ content/index.js
@@ -1,6 +1,12 @@
+$('body').ready(function() {
+    $.ajax({
+        url: url,
+        data: data,
+        success: success,
+        dataType: dataType
+    });
 
-$('body').ready( function () {
-  $('body').prepend('<p id="screen-reader-summary">This is placeholder screen-reader text which will be used to display the summary of the page.</p>');
-  
-  $('#screen-reader-summary').focus();
+    $('body').prepend('<p id="screen-reader-summary">This is placeholder screen-reader text which will be used to display the summary of the page.</p>');
+
+    $('#screen-reader-summary').focus();
 });
