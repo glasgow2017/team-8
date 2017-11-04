bf05164cf91589eab61b49092649d816033c02a4
Adam Hirst <adam@ahirst.com>
2017-11-04 19:03:54 +0000

    Convert selectors to JQuery entities

--- content/index.js
+++ content/index.js
@@ -3,6 +3,9 @@ var config = {
   enabled: true
 };
 
+// Prefix of generated elements
+var elPrefix = 't8-cfg_';
+
 // Page structure definitions
 var structureDefs = {
   header: [ 'header', '.header', 'div[role="banner"]' ],
@@ -15,6 +18,10 @@ function getConfig() {
   chrome.storage.sync.get('t8-config', function(obj) {
     config = obj;
   }.bind(this));
+}
+
+function getPluginEl(name) {
+  return $("#" + elPrefix + name);
 }
 
 // Remove empty elements from array
@@ -32,31 +39,24 @@ Array.prototype.removeAllEmpty = function() {
 Array.prototype.toJQueryEntities = function() {
   return this.map(function (x) {
     var c = $(x);
-    c.removeAllEmpty(); // Remove empty
-  });
+  }).removeAllEmpty();
 }
 
 $('body').ready( function () {
-  //$('body').prepend('<p id="screen-reader-summary">This is placeholder screen-reader text which will be used to display the summary of the page.</p>');
-
-  $('#screen-reader-summary').focus();
-
-  // Header management
-  var headerSelectors = ['header', 'div[role="banner"]'];
-  // Find a header to prepend
-  var header = $(headerSelectors[0]);
-  for (var i = 0; i < headerSelectors.length; i++) {
-    if (header == undefined) {
-      header = headerSelectors[i];
-    } else {
-      break;
-    }
+  // Populate structure
+  var structure = {
+    header: structureDefs.header.toJQueryEntities(),
+    nav: structureDefs.header.toJQueryEntities(),
+    footer: structureDefs.header.toJQueryEntities(),
   }
 
-  hideHeaders();
+  // Add header section
+  // $('body').prepend( '<div id="' + elPrefix + 'header" style="width: 100%; padding: 20px; text-align: center; background-color: #6ab7ff; color: #fff; position: fixed; z-index: 100000;"></div>' );
+  // 
+  // var cfgHeader = getPluginEl('header');
+  //
+  //
+  //
+  // cfgHeader.append( '<h1>Code For Good</h1> ' )
 
-  // Select a header
-  header.before( '<div id="header-show" style="width: 100%; padding: 20px; text-align: center; background-color: #6ab7ff"><button id="header-show-btn" style="padding: 20px; background-color: #1e88e5; color: #fff; font-size: 0.875rem; font-weight: 700; display: inline-block; min-width: 200px; text-transform: uppercase; border-radius: 9999em; font-family: Helvetica, Arial, sans-serif; text-align: center; vertical-align: middle; border: 0 none; outline: none;">Display header</button></div>' );
-
-  $('#header-show-btn').click(showHeaders);
 });
