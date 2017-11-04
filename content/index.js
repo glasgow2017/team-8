6f92b27a1130c758ce828bb9d0cba9460532e662
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-04 17:01:09 +0000

    Attempt to force the screen reader to read the page summary fist

--- content/index.js
+++ content/index.js
@@ -1,1 +1,6 @@
-$('body').prepend('<p>This is placeholder screen-reader text which will be used to display the summary of the page.</p>');
+
+$('body').ready( function () {
+  $('body').prepend('<p id="screen-reader-summary">This is placeholder screen-reader text which will be used to display the summary of the page.</p>');
+  
+  $('#screen-reader-summary').focus();
+});
