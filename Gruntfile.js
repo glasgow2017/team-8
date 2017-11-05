f9f23966a917df1d17ec98256e2e1102caa3239d
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-05 01:24:51 +0000

    Revert revert pull request (I promise this is not for the commit count)

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {
+          'content/script.min.js': [
+            'dev-js/extract_important_sentence.js',
+            'dev-js/api_calls.js',
+            'dev-js/parser.js',
+            'dev-js/index.js'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify']);
+};
