f151d10d0e5e660a917476fb4202a8630688d4f3
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-05 09:50:49 +0000

    Add substring to prevent overflowing getSummary

--- dev-js/page_flow.js
+++ dev-js/page_flow.js
@@ -37,11 +37,10 @@ function findAndReplace(searchText, replacement, searchNode) {
 $('body').ready(function() {
     displayTopBanner();
 
-    const strPageText = extractTextFromPage($('body'));
+    const strPageText = extractTextFromPage($('body')).substring(0, 2000);
     const strPageImage = extractImageFromPage($('body'));
 
-    //const sentences = getSummary(strPageText, 'Page text', 4); // TMP
-    const sentences = ["US President Donald Trump has arrived in Japan at …ietnam and the Philippines also on his itinerary.", "President Trump arrived at the Yokota US Air Force base west of Tokyo on Sunday morning.", "Bilateral meetings with Mr Abe↵↵Tuesday, 7 Novembe…South Korea for talks with President Moon Jae-in.", "Mr Trump will also address the National Assembly↵↵… gathering that includes the US, India and Russia"]; // TMP
+    const sentences = getSummary(strPageText, 'Page text', 4); // TMP
     console.log(sentences);
 
     $(document).ready(function() {
