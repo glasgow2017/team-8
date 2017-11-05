9f2b589959b3579bae23ed098ce180801919840b
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-05 01:51:46 +0000

    Move add_top_banner.js's code inside a function

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,11 @@ module.exports = function(grunt) {
       my_target: {
         files: {
           'content/script.min.js': [
+            'dev-js/add_top_banner.js',
             'dev-js/extract_important_sentence.js',
             'dev-js/api_calls.js',
             'dev-js/parser.js',
-            'dev-js/index.js'
+            'dev-js/page_flow.js',
           ]
         }
       }
