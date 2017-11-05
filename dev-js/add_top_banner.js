beb3975d338dff9d88aa462bd32e06405f57e7ef
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-05 06:10:55 +0000

    Fix some ESLint issues with eslint --fix

--- dev-js/add_top_banner.js
+++ dev-js/add_top_banner.js
@@ -1,29 +1,29 @@
 const displayTopBanner = function fDisplayTopBanner() {
   // Global config
-  var config = {
-    enabled: true
+  const config = {
+    enabled: true,
   };
-  
+
   // Prefix of generated elements
-  var elPrefix = 't8-cfg_';
-  
+  const elPrefix = 't8-cfg_';
+
   // Page structure definitions
-  var structureDefs = {
-    header: [ 'header', '.header', 'div[role="banner"]' ],
-    nav: [ 'nav', '.nav', 'div[role="navigation"]' ],
-    footer: [ 'footer', '.footer', 'div[role="contentinfo"]' ]
-  }
-  
+  const structureDefs = {
+    header: ['header', '.header', 'div[role="banner"]'],
+    nav: ['nav', '.nav', 'div[role="navigation"]'],
+    footer: ['footer', '.footer', 'div[role="contentinfo"]'],
+  };
+
   // i18n support ;)
-  var strings = {
+  const strings = {
     en: {
       widget_title: 'Quick access',
       summary: 'Summary',
       structure: 'Page structure',
-    }
-  }
-  
-  var styles =`<style>
+    },
+  };
+
+  const styles = `<style>
     #t8-cfg_header {
       width: 300px;
       padding: 20px;
@@ -53,74 +53,64 @@ const displayTopBanner = function fDisplayTopBanner() {
       font-weight: 100;
     }
   </style>`;
-  
-  // Get the config
-  function getConfig() {
-    chrome.storage.sync.get('t8-config', function(obj) {
-      config = obj['t8-config'];
-    }.bind(this));
-  }
-  
+
   function getPluginEl(name) {
-    return $("#" + elPrefix + name);
+    return $(`#${elPrefix}${name}`);
   }
-  
+
   // Remove empty elements from array
-  Array.prototype.removeAllEmpty = function() {
-    for (var i = 0; i < this.length; i++) {
+  Array.prototype.removeAllEmpty = function () {
+    for (let i = 0; i < this.length; i++) {
       if (this[i] == undefined) {
         this.splice(i, 1);
         i--;
       }
     }
     return this;
-  }
-  
+  };
+
   // Get page elements from selectors
-  Array.prototype.toJQueryEntities = function() {
-    return this.map(function (x) {
-      var c = $(x);
+  Array.prototype.toJQueryEntities = function () {
+    return this.map((x) => {
+      const c = $(x);
     }).removeAllEmpty();
-  }
-  
+  };
+
   function setSummary(text) {
     getPluginEl('summary').text(text);
   }
-  
+
   function addNavigationItem(title, anchor) {
-    getPluginEl('nav').append( '<a href="#' + anchor + '"><li>' + title + '</li></a>' );
+    getPluginEl('nav').append(`<a href="#${anchor}"><li>${title}</li></a>`);
   }
-  
+
   function addAnchorToElement(name, el) {
-    el.before( '<a name="' + elPrefix + name + '" />' );
+    el.before(`<a name="${elPrefix}${name}" />`);
     return elPrefix + name;
   }
-  
-  $('body').ready( function () {
+
+  $('body').ready(() => {
     if (config.enabled) {
       // Populate structure
-      var structure = {
+      const structure = {
         header: structureDefs.header.toJQueryEntities(),
         nav: structureDefs.header.toJQueryEntities(),
         footer: structureDefs.header.toJQueryEntities(),
-      }
-  
+      };
+
       // Add header section
-      $('body').prepend( styles + '<div id="' + elPrefix + 'header" tabindex="1"></div>' );
-  
-      var cfgHeader = getPluginEl('header');
-      cfgHeader.append( '<h3>' + strings.en.widget_title + '</h3> ' );
-      cfgHeader.append( '<h4>' + strings.en.summary + '</h4><p id="' + elPrefix + 'summary"></p>' );
-      cfgHeader.append( '<h4>' + strings.en.structure + '</h4><ul id="' + elPrefix + 'nav"></ul>' );
-  
-      if (structure.header.length > 0)
-        addNavigationItem('Jump to Header', addAnchorToElement(structure.header[0]));
-  
-      if (structure.nav.length > 0)
-        addNavigationItem('Jump to Navigation', addAnchorToElement(structure.nav[0]));
-  
-      if (structure.footer.length > 0)
-        addNavigationItem('Jump to Footer', addAnchorToElement(structure.footer[0]));
+      $('body').prepend(`${styles}<div id="${elPrefix}header" tabindex="1"></div>`);
+
+      const cfgHeader = getPluginEl('header');
+      cfgHeader.append(`<h3>${strings.en.widget_title}</h3> `);
+      cfgHeader.append(`<h4>${strings.en.summary}</h4><p id="${elPrefix}summary"></p>`);
+      cfgHeader.append(`<h4>${strings.en.structure}</h4><ul id="${elPrefix}nav"></ul>`);
+
+      if (structure.header.length > 0) { addNavigationItem('Jump to Header', addAnchorToElement(structure.header[0])); }
+
+      if (structure.nav.length > 0) { addNavigationItem('Jump to Navigation', addAnchorToElement(structure.nav[0])); }
+
+      if (structure.footer.length > 0) { addNavigationItem('Jump to Footer', addAnchorToElement(structure.footer[0])); }
     }
   });
-}
+};
--- dev-js/page_flow.js
+++ dev-js/page_flow.js
@@ -1,63 +1,58 @@
 function findAndReplace(searchText, replacement, searchNode) {
-    if (!searchText || typeof replacement === 'undefined') {
-        // Throw error here if you want...
-        return;
+  if (!searchText || typeof replacement === 'undefined') {
+    // Throw error here if you want...
+    return;
+  }
+  let regex = typeof searchText === 'string' ?
+      new RegExp(searchText, 'g') : searchText,
+    childNodes = (searchNode || document.body).childNodes,
+    cnLength = childNodes.length,
+    excludes = 'html,head,style,title,link,meta,script,object,iframe';
+  while (cnLength--) {
+    var currentNode = childNodes[cnLength];
+    if (currentNode.nodeType === 1 &&
+            (`${excludes},`).indexOf(`${currentNode.nodeName.toLowerCase()},`) === -1) {
+      arguments.callee(searchText, replacement, currentNode);
     }
-    var regex = typeof searchText === 'string' ?
-        new RegExp(searchText, 'g') : searchText,
-        childNodes = (searchNode || document.body).childNodes,
-        cnLength = childNodes.length,
-        excludes = 'html,head,style,title,link,meta,script,object,iframe';
-    while (cnLength--) {
-        var currentNode = childNodes[cnLength];
-        if (currentNode.nodeType === 1 &&
-            (excludes + ',').indexOf(currentNode.nodeName.toLowerCase() + ',') === -1) {
-            arguments.callee(searchText, replacement, currentNode);
-        }
-        if (currentNode.nodeType !== 3 || !regex.test(currentNode.data)) {
-            continue;
-        }
-        var parent = currentNode.parentNode,
-            frag = (function() {
-                var html = currentNode.data.replace(regex, replacement),
-                    wrap = document.createElement('div'),
-                    frag = document.createDocumentFragment();
-                wrap.innerHTML = html;
-                while (wrap.firstChild) {
-                    frag.appendChild(wrap.firstChild);
-                }
-                return frag;
-            })();
-        parent.insertBefore(frag, currentNode);
-        parent.removeChild(currentNode);
+    if (currentNode.nodeType !== 3 || !regex.test(currentNode.data)) {
+      continue;
     }
+    let parent = currentNode.parentNode,
+      frag = (function () {
+        let html = currentNode.data.replace(regex, replacement),
+          wrap = document.createElement('div'),
+          frag = document.createDocumentFragment();
+        wrap.innerHTML = html;
+        while (wrap.firstChild) {
+          frag.appendChild(wrap.firstChild);
+        }
+        return frag;
+      }());
+    parent.insertBefore(frag, currentNode);
+    parent.removeChild(currentNode);
+  }
 }
 
 
-$('body').ready(function() {
-    displayTopBanner();
+$('body').ready(() => {
+  displayTopBanner();
 
-    const strPageText = extractTextFromPage($('body'));
+  const strPageText = extractTextFromPage($('body'));
 
-    const sentences = getSummary(strPageText, 'Page text', 4); // TMP
-    //const sentences = ["US President Donald Trump has arrived in Japan at …ietnam and the Philippines also on his itinerary.", "President Trump arrived at the Yokota US Air Force base west of Tokyo on Sunday morning.", "Bilateral meetings with Mr Abe↵↵Tuesday, 7 Novembe…South Korea for talks with President Moon Jae-in.", "Mr Trump will also address the National Assembly↵↵… gathering that includes the US, India and Russia"]; // TMP
-    console.log(sentences);
-
-
-    $(document).ready(function() {
-        var html = '<ul>';
-        const $body = $('body');
-        for (var i = 0; i < sentences.length; i++) {
-
-            findAndReplace(sentences[i], '<a name="Sentence"' + i + '"/a');
-
-            html += '<li>' + sentences[i] + '</li>';
-
-        }
-        html += '</ul>';
-        $('#t8-cfg_header').prepend(html);
-    });
+  // const sentences = getSummary(strPageText, 'Page text', 4); // TMP
+  const sentences = ["US President Donald Trump has arrived in Japan at …ietnam and the Philippines also on his itinerary.", "President Trump arrived at the Yokota US Air Force base west of Tokyo on Sunday morning.", "Bilateral meetings with Mr Abe↵↵Tuesday, 7 Novembe…South Korea for talks with President Moon Jae-in.", "Mr Trump will also address the National Assembly↵↵… gathering that includes the US, India and Russia"]; // TMP
+  console.log(sentences);
 
 
+  $(document).ready(() => {
+    let html = '<ul>';
+    const $body = $('body');
+    for (let i = 0; i < sentences.length; i++) {
+      findAndReplace(sentences[i], `<a name="Sentence"${i}"/a`);
 
+      html += `<li>${sentences[i]}</li>`;
+    }
+    html += '</ul>';
+    $('#t8-cfg_header').prepend(html);
+  });
 });
