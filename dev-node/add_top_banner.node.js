b7363c78049592157fef75aa0b8d4bc6ace7ccf6
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-05 01:24:51 +0000

    Revert revert pull request (I promise this is not for the commit count)

--- dev-node/add_top_banner.node.js
+++ dev-node/add_top_banner.node.js
@@ -4,7 +4,7 @@
  */
 $('body').ready( function () {
   $('body').prepend('<div id="screen-reader-summary"><a href="#scb-end" id="gsw17-8-sr-read">Read page from the start</a></div><div id="scb-end"></div>');
-
+  
   $('#screen-reader-summary').focus();
 
   $(document).bind('keyup', 'ctrl+y', function () {
