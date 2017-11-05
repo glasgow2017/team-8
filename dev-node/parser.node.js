9082e92198e63f5eb72743b018038efb51bf45b8
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-05 06:10:55 +0000

    Fix some ESLint issues with eslint --fix

--- dev-node/parser.node.js
+++ dev-node/parser.node.js
@@ -3,7 +3,7 @@ extractor = require('unfluff-without-node');
 global.extractTextFromPage = function fExtractTextFromPage($body) {
   data = extractor.lazy($body.html());
   plane = data.text();
-  plane.replace('.','. ');
-  plane.replace('\n',' ');
-  return(plane);
-}
+  plane.replace('.', '. ');
+  plane.replace('\n', ' ');
+  return (plane);
+};
