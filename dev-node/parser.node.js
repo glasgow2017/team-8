2eed0c78632c315184d555578ea14e9bd1ce5af9
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-05 01:44:56 +0000

    Move parse.node.js's code inside a function

--- dev-node/parser.node.js
+++ dev-node/parser.node.js
@@ -1,9 +1,11 @@
 extractor = require('unfluff-without-node');
 
-$('body').ready(function() {
-    data = extractor.lazy($("body").html());
-    plane = data.text();
-    plane.replace('.','. ');
-    plane.replace('\n',' ');
-    return(plane);
-});
+const extractReadableTextFromPage = function extractTextFromPage() {
+    $('body').ready(function() {
+        data = extractor.lazy($("body").html());
+        plane = data.text();
+        plane.replace('.','. ');
+        plane.replace('\n',' ');
+        return(plane);
+    });
+}
