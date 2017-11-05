30dc7ad7d9f4b5fe37e373b57471ecfc6855da12
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-05 08:39:50 +0000

    Add method to get page sections

--- dev-js/page_flow.js
+++ dev-js/page_flow.js
@@ -41,7 +41,7 @@ $('body').ready(() => {
 
   // const sentences = getSummary(strPageText, 'Page text', 4); // TMP
   const sentences = ["US President Donald Trump has arrived in Japan at …ietnam and the Philippines also on his itinerary.", "President Trump arrived at the Yokota US Air Force base west of Tokyo on Sunday morning.", "Bilateral meetings with Mr Abe↵↵Tuesday, 7 Novembe…South Korea for talks with President Moon Jae-in.", "Mr Trump will also address the National Assembly↵↵… gathering that includes the US, India and Russia"]; // TMP
-  console.log(sentences);
+  // console.log(sentences);
 
 
   $(document).ready(() => {
