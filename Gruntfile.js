1322c68e3712dc3cf7278aa7083e4d567a03b272
Louis-Marie Matthews <louismarie.matthews@outlook.fr>
2017-11-05 02:20:23 +0000

    Clean up some JavaScript, fix bug with extractText

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function(grunt) {
         files: {
           'content/script.min.js': [
             'dev-js/add_top_banner.js',
-            'dev-js/extract_important_sentence.js',
             'dev-js/api_calls.js',
             'dev-js/parser.js',
             'dev-js/page_flow.js',
--- dev-js/page_flow.js
+++ dev-js/page_flow.js
@@ -1,1 +1,2 @@
+extractTextFromPage();
 displayTopBanner();
